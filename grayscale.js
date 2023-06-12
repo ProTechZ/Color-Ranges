09fee5768236314ec6d840c3f8da3e0666b8756b
ProTechZ <kingofbeastz777@gmail.com>
2023-06-12 17:00:43 +1000

    Added final touches

--- grayscale.js
+++ grayscale.js
@@ -2,18 +2,18 @@
 grayscaleSection = document.getElementById('section-grayscale')
 
 for (let i = 0; i < 101; i++) {
-    m = document.createElement('div');
-    m.style.backgroundColor = `hsl(0, 0%, ${i}%)`;
-    m.setAttribute('class', 'grayscale-ranges');
+    colour = document.createElement('div');
+    colour.style.backgroundColor = `hsl(0, 0%, ${i}%)`;
+    colour.setAttribute('class', 'grayscale-colour');
 
-    n = document.createElement('p');
-    n.textContent = i;
+    colourNum = document.createElement('p');
+    colourNum.textContent = i;
 
     if (i < 60) {
-        n.style.color = `white`;
-        m.style.borderColor = 'white';
+        colourNum.style.color = `white`;
+        colour.style.borderColor = 'white';
     } 
 
-    m.appendChild(n);
-    grayscaleSection.appendChild(m);
+    colour.appendChild(colourNum);
+    grayscaleSection.appendChild(colour);
 }
